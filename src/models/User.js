0163e230efd5b843c504daf4aeead2c310756e1c
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-04-13 13:53:13 -0400

    wip

--- src/models/User.js
+++ src/models/User.js
@@ -8,7 +8,13 @@ const User = sequelize.define('user', {
     password: { type: DataTypes.STRING, allowNull: false },
     country: { type: DataTypes.STRING, allowNull: false },
     image: { type: DataTypes.TEXT, allowNull: false },
-    isVerified: { type: DataTypes.BOOLEAN, allowNull: false },
+    isVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
 }, { underscored: true });
+
+User.prototype.toJSON = function () {
+   const values = { ...this.get() };
+   delete values.password ;
+   return values;
+};
 
 module.exports = User;
